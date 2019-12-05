63ca1fbeac93a1fc5de737f7267867a32ce34d82
Moises Fausto <moises_system@yahoo.com.br>
2019-12-05 14:26:07 -0300

    Novo Envio por Moises[Work]

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+    //OwlCarousel
+    $(".owl-carousel").owlCarousel({
+        autoplay:false,
+        loop:false,
+        margin:10,
+        nav:false,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:false
+            }
+                        
+        }
+    });
+    
+
+});
