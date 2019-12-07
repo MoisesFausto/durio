0bbc4439e95c14d5fd55d51365de5b7df22c6070
Moises Fausto <moises_system@yahoo.com.br>
2019-12-06 23:07:20 -0300

    Novas att e configurações em JS

--- js/main.js
+++ js/main.js
@@ -1,5 +1,26 @@
 $(document).ready(function(){
 
+    
+    //Config page eventos
+    $('.btn-collapse').click(function(){        
+        $('.collapse').collapse('hide');       
+        //$(this).css('filter', 'brightness(.35) hue-rotate(190deg)');        
+    });    
+
+    
+    //Menu fixo
+    var offset = $('#header').offset().top;
+    var $meuMenu = $('#header');
+    $(document).on('scroll', function () {
+        if (offset < $(window).scrollTop()) {
+            $meuMenu.addClass('fixed-top');
+            $('#btn-contato-header').addClass('btn-padrao');
+        } else {
+            $meuMenu.removeClass('fixed-top');
+            $('#btn-contato-header').removeClass('btn-padrao');
+        }    
+    });
+
     //OwlCarousel
     $(".owl-carousel").owlCarousel({
         autoplay:false,
@@ -11,9 +32,11 @@ $(document).ready(function(){
             0:{
                 items:1,
                 nav:false
-            }
-                        
+            }                        
         }
     });
+
+
+    
 
 });
